ecb843e6563c48b660b9deae126c0fa3b3682d2f
AndyO <andyortlieb+git@gmail.com>
2013-05-02 15:23:24 -0500

    meh

---
+++ README.md
@@ -0,0 +1,15 @@
+
+##
+
+## Moving parts
+
+* Data
+	* User
+		* Auth stuff/ profile
+		* Identity
+			* Service type
+			* Service info/profile
+
+* Libraries, Dependencies
+	* "github.com/thoj/go-ircevent"
+	* 
--- irckd.go
+++ irckd.go
@@ -2,69 +2,151 @@ package main
 
 import (
 	"github.com/thoj/go-ircevent"
+	"net/http"
 	//"github.com/bmizerany/pq"
 	"fmt"
-	"bufio"
-	"os"
+	"time"
+	"io"
+	// "bufio"
+	// "os"
 )
 
-type Channel struct {
-	name		string
-	history 	[]string
+func stub(msg string){
+	fmt.Println("STUB: ", msg)
+}
+func erro(msg string){
+	fmt.Println("ERROR: ", msg)
+}
+
+type User struct {
+	username	string
+	identities	[]*Identity
 }
 
 type Identity struct {
 	servertype	string
 	serveraddr	string
-	username	string
+	nick	string
+	enabled		bool
 	channels	[]Channel
-	connection	irc.Connection
+	connection	*irc.Connection
+	user 		*User
+
 }
 
-type User struct {
-	username	string
-	identities	[]Identity
+type Channel struct {
+	name		string
+	history 	*[]string
 }
 
-func Create() *irc.Connection {
-	//irccon := IRC("go-eventirc", "go-eventirc")
-	irccon := irc.IRC("dingolvr", "dingolvr")
+func (usr *User) AddIdentity(stype string,addr string,nick string,enabled bool) *Identity{
+	idt := Identity{
+		servertype:stype,
+		serveraddr:addr,
+		nick:nick,
+		enabled:enabled,
+		user:usr,
+	}
+	usr.identities = append(usr.identities, &idt)
+	return &idt
+}
+
+func (idt *Identity) JoinChannels(){
+	stub("Joining Channels")
+	idt.connection.Join("#dingolove")
+}
+
+func (idt *Identity) Connect() *irc.Connection{
+	// Initialize a connection
+	irccon := irc.IRC(idt.nick, idt.user.username)
 	irccon.VerboseCallbackHandler = true
+	idt.connection = irccon
+
+	// Really connect
+	err := irccon.Connect(idt.serveraddr)
+	if err != nil{
+		erro(fmt.Sprintf("Cannot connect to `", idt.serveraddr, "` for user `", idt.user.username))
+	}
+
+	// Manage it
+	irccon.AddCallback("001", func(e *irc.Event){idt.JoinChannels()})
+	go func(){
+		irccon.Loop()
+	}()
 	return irccon
 }
 
-func Connect(irccon *irc.Connection) {
-	// Connect
-	err := irccon.Connect("irc.freenode.net:6667")
-	if err != nil {
-		fmt.Println("Can't connect to freenode.")
+func (idt *Identity) AddChannel(msg string){
+	cn := Channel{
+		name:msg,
 	}
-
-	// Join all of the channels.
-	irccon.AddCallback("001", func(e *irc.Event) { irccon.Join("#dingolove") })
-	
-	// Hang out.
-	irccon.Loop()
+	idt.channels = append(idt.channels, cn)
+	stub("TODO: Connect to channel if we are already connected to the host")
 }
 
-func main(){
+func initUsers(){
+
+	stub("Set up some users")
+	guyA := User{username: "dingolvrA"}
+	idtA1 := guyA.AddIdentity(
+		"irc",
+		"dv.opasc.net:6667",
+		"dingolvrA1",
+		true,
+	)
+	idtA2 := guyA.AddIdentity(
+		"irc",
+		"dv.opasc.net:6667",
+		"dingolvrA2",
+		true,
+	)
+	fmt.Println(20, idtA1.channels, guyA.identities[0].channels)
+	idtA1.AddChannel("#dingolove")
+	fmt.Println(21, idtA1.channels, guyA.identities[0].channels)
+	idtA2.AddChannel("#dingolove")
+
+	idtA1.Connect()
+	idtA2.Connect()
+
+/*	// Just for now... loop with readstring until we know how to be a real good daemon.
+
 	br := bufio.NewReaderSize(os.Stdin, 512)
-	irccon := Create();
-
-	go Connect(irccon)
-
 	for {
 		msg, err := br.ReadString('\n')
 		if err != nil {
-			irccon.Quit()
 			break;
 		}
 		if msg[:5] == "/quit" {
-			irccon.Quit()
 			return
 		}
 		fmt.Println("#dingolove/dingolvr: ", msg)
-		irccon.Privmsg("#dingolove", msg+"\n")
+		idtA1.connection.Privmsg("AcidTrucks", msg+"\n")
+		idtA2.connection.Privmsg("#dingolove", msg+"\n")
+		//irccon.Privmsg("#dingolove", msg+"\n")
+	}	
+*/
+}
+
+func myHandler(writer http.ResponseWriter, r *http.Request){
+	io.WriteString(writer, "sup!\n")
+}
+
+func initHttp(){
+	s := &http.Server{
+		Addr: 	":7776",
+		Handler: 	myHandler,
+		ReadTimeout:	10*time.Second,
+		WriteTimeout: 	10*time.Second,
+		MaxHeaderBytes:	1 << 20, // ??
 	}
 
+	s.ListenAndServe()
 }
+
+func main(){
+	stub("Starting irckd")
+
+	initUsers()
+	initHttp()
+}
+
